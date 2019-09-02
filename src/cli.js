daedb374a63a2b478b2af023166646af813151a5
towercity <matthewjnerger3@gmail.com>
2019-09-01 22:22:00 -0400

    Add script selection function

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import arg from 'arg';
+import inquirer from 'inquirer';
 
 import correctDeck from '../src/scripts/correctDecks';
 import changeSubs from '../src/scripts/changeSubs';
@@ -19,9 +20,31 @@ const  parseArgumentsIntoOptions = (rawArgs) => {
     };
 }
 
+async function selectScript(options) {
+    // if a command has been entered, proceed
+    if (options.cmd) {
+        return { 
+            ...options
+        }
 
+    // otherwise, a selection process is run
+    } else {
+        // select possible commands
+        const answers = await inquirer.prompt({
+            type: 'list',
+            name: 'cmd',
+            message: 'Please select a script to run',
+            choices: ['00change', 'correct decks']
+        });
+        return {
+            ...options,
+            cmd: answers.cmd
+        }
+    }
+}
 
-export function cli(args) {
+export async function cli(args) {
     let options = parseArgumentsIntoOptions(args);
+    options = await selectScript(options);
     console.log(options);
 }
