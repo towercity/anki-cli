707c5b47b5abdf1b4cb5ca0c63d5319607fbbcbe
towercity <matthewjnerger3@gmail.com>
2019-09-01 22:07:37 -0400

    Add argument importing logic

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,27 @@
+import arg from 'arg';
+
 import correctDeck from '../src/scripts/correctDecks';
 import changeSubs from '../src/scripts/changeSubs';
 
+const  parseArgumentsIntoOptions = (rawArgs) => {
+    const args = arg(
+      {
+        '--help': Boolean,
+        '-h': '--help'
+      },
+      {
+        argv: rawArgs.slice(2),
+      }
+    );
+    return {
+      help: args['--help'] || false,
+      cmd: args._[0]
+    };
+}
+
+
+
 export function cli(args) {
-    console.log(args);
-    
+    let options = parseArgumentsIntoOptions(args);
+    console.log(options);
 }
