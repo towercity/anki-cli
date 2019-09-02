da457fea28ff3af1ab5fb163d5ad3b2ec7132454
towercity <matthewjnerger3@gmail.com>
2019-09-02 14:30:06 -0400

    Add script naming object

---
+++ src/scripts/all.js
@@ -0,0 +1,9 @@
+import changeSubs from '../scripts/changeSubs';
+import correctDeck from '../scripts/correctDecks';
+
+const cliScripts = {
+    '00change': changeSubs,
+    'correct-decks': correctDeck
+}
+
+export default cliScripts;
