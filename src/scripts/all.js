eae71e1e6b204a60d361a0fd4d03686b548c180f
Matt Nerger <matthewjnerger3@gmail.com>
2019-09-30 16:44:52 -0400

    Remove faulty dependency

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -2,10 +2,18 @@ import changeSubs from '../scripts/changeSubs';
 import correctDeck from '../scripts/correctDecks';
 import notesAddLoop from '../scripts/notesAddLoop';
 
+const test = () => {
+    console.log('ttttest');
+    const text = '僕の私語多は難しかったので、やんでいた。';
+
+    console.log(text);
+}
+
 const cliScripts = {
     '00change': changeSubs,
     // 'correct-decks': correctDeck, // hide it b/c i'm not using it now
-    'notes-add-loop': notesAddLoop
+    'notes-add-loop': notesAddLoop,
+    'test': test
 }
 
 export default cliScripts;
