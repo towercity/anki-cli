7268877733c7b222a83f1dea3b4c436a43c9588d
Matt Nerger <matthewjnerger3@gmail.com>
2019-10-02 09:36:51 -0400

    Test muliti-tag add functionality

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -2,11 +2,14 @@ import changeSubs from '../scripts/changeSubs';
 import correctDeck from '../scripts/correctDecks';
 import notesAddLoop from '../scripts/notesAddLoop';
 
-import path from 'path';
+import Anki from '../services/Anki';
 
 const test = () => {
     console.log('ttttest');
     const text = '僕の私語多は難しかったので、やんでいた。';
+
+    const temp_id = [1339252521447];
+    // Anki.addTags('00change review', temp_id);
 
     console.log(text);
 }
