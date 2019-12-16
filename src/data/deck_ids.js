3370c1dabbd83bbf4728dec4721d87e4622fba02
Matt Nerger <matthewjnerger3@gmail.com>
2019-12-16 10:03:41 -0500

    update deck anmes

--- src/data/deck_ids.js
+++ src/data/deck_ids.js
@@ -1,8 +1,8 @@
 const DECK_IDS = {
-    subs: "01 - 日本語::01 - カード::02 - subs_bank",
-    listening: "01 - 日本語::01 - カード::01 - Listening", 
-    other: "01 - 日本語::01 - カード::02 - Other",
-    main: "01 - 日本語::01 - カード::01 - Cards"
+    subs: "All::01 - 日本語::01 - カード::02 - subs_bank",
+    listening: "All::01 - 日本語::01 - カード::01 - Listening", 
+    other: "All::01 - 日本語::01 - カード::02 - Other",
+    main: "All::01 - 日本語::01 - カード::01 - Cards"
 }
 
 export default DECK_IDS;
